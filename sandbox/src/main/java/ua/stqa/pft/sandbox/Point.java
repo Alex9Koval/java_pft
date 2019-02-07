270f79b07b5d99aa2390b290cdf67283398a3236
Alex9Koval <47238612+Alex9Koval@users.noreply.github.com>
2019-02-07 23:26:10 +0200

    наброски ДЗ

--- sandbox/src/main/java/ua/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Point.java
@@ -3,13 +3,15 @@ package ua.stqa.pft.sandbox;
 public class Point {
 
     public static void main(String[] args) {
-        double x = 5;
-        double y = 6;
+        double x = 5.6;
+        double y = 6.5;
+        double a = Math.sqrt(5.6 * 6.5);
 
-        System.out.println("Представления точек на двумерной плоскости с их координатами "  + x + " и " + y + " = " + distance(x,y)   );
+        System.out.println("Представления точек на двумерной плоскости с их координатами "  + x + " и " + y + " = " + sqrt(x,y)   );
     }
 
-    public static double distance(double x, double y){
+    public static double sqrt (double x, double y){
         return x * y;
     }
+    // AB = √AC2 + BC2
 }
