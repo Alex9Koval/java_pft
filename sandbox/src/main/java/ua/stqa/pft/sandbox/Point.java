c05516491eec7c592ca921c4f27a80bb546ab321
Alex9Koval <47238612+Alex9Koval@users.noreply.github.com>
2019-02-08 20:16:42 +0200

    Задание №2. Расстояние между двумя точками

--- sandbox/src/main/java/ua/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Point.java
@@ -3,15 +3,13 @@ package ua.stqa.pft.sandbox;
 public class Point {
 
     public static void main(String[] args) {
-        double x = 5.6;
-        double y = 6.5;
-        double a = Math.sqrt(5.6 * 6.5);
 
-        System.out.println("Представления точек на двумерной плоскости с их координатами "  + x + " и " + y + " = " + sqrt(x,y)   );
+        Root r = new Root(5,3,6,4);
+
+        System.out.println("Расстояние между точками на двумерной плоскости " + "= " +area(r));
     }
 
-    public static double sqrt (double x, double y){
-        return x * y;
+    public static double area (Root r){
+        return Math.sqrt(Math.pow((r.x2 - r.x1),2 )+ Math.pow((r.y2-r.y1),2));
     }
-    // AB = √AC2 + BC2
 }
---
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Root.java
@@ -0,0 +1,17 @@
+package ua.stqa.pft.sandbox;
+
+public class Root {
+
+    public double x1;
+    public double x2;
+    public double y1;
+    public double y2;
+
+    public Root(double x1, double y1, double x2, double y2){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+}
