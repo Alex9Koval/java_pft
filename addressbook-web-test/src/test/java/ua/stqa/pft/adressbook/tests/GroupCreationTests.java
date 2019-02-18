887ee897153b615ea2e431bb69e58fc2b1aca22c
Alex9Koval <47238612+Alex9Koval@users.noreply.github.com>
2019-02-18 22:39:24 +0200

    Выделяем специализированные классы-помощники

--- addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -8,11 +8,11 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.gotoGroupTest();
-    app.initGroupGreation();
-    app.fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
+    app.getNavigationHelper().gotoGroupTest();
+    app.getGroupHelper().initGroupGreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }
--- addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/tests/GroupDeletionTests.java
@@ -6,10 +6,10 @@ public class GroupDeletionTests extends TestBase{
 
   @Test
   public void testGroupDelitions() throws Exception {
-    app.gotoGroupTest();
-    app.selectGroup();
-    app.deleteSelectedGroups();
-    app.returnToGroupPage();
+    app.getNavigationHelper().gotoGroupTest();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }
