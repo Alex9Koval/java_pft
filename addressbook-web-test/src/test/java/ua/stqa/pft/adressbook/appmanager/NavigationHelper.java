5d5291b3ebcfe40d1961e51e40c2c7b54804a035
Alex9Koval <47238612+Alex9Koval@users.noreply.github.com>
2019-02-19 21:49:37 +0200

    Реализация двухуровневой архитектуры тестового набора

--- addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/appmanager/NavigationHelper.java
@@ -3,14 +3,13 @@ package ua.stqa.pft.adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver wd;
+public class NavigationHelper extends  HelperBase{
 
     public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void gotoGroupTest() {
-      wd.findElement(By.linkText("groups")).click();
+     click(By.linkText("groups"));
     }
 }
