5c08aed328c3e9f23a4672c84a760088021b1394
Alex9Koval <47238612+Alex9Koval@users.noreply.github.com>
2019-02-25 21:39:55 +0200

    Проверка условий при помощи конструкции if-then-else

--- sandbox/src/test/java/ua/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ua/stqa/pft/sandbox/PointTest.java
@@ -8,6 +8,6 @@ public class PointTest {
     @Test
     public void testArea(){
         Root r = new Root(5,3,6,4 );
-        Assert.assertEquals(r.area(), 1.4142135623730951);
+        Assert.assertEquals(r.distance(), 1.4142135623730951);
     }
 }
