d417e57f059ab67556e1f64610b6c58cc295b4cc
Alex9Koval <47238612+Alex9Koval@users.noreply.github.com>
2019-03-11 21:33:20 +0200

    Запуск тестов в разных браузерах Firefox и Chrome

--- addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ua/stqa/pft/adressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ua.stqa.pft.adressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ua.stqa.pft.adressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
